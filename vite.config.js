fdf9c748d42a5aae59ca08b6d3cf5e9ba766075f
Nguyen Thanh Trieu <trieukon1011@gmail.com>
2025-07-08 20:04:01 +0700

    change from abs path to ref path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/web-design-contest-website/',
   server: {
     allowedHosts: ['.konnn04.live', 'localhost'],
     port: 3000,
